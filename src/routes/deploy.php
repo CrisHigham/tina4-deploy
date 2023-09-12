6da9c3b26ea5760695350c999a877d3d1961a44b
Cris Higham <cris@justblink.co.za>
2023-09-12 17:52:38 +0200

    remove deploy

--- src/routes/deploy.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-Tina4\Post::add("/test/deploy", function(Tina4\Response $response, Tina4\Request $request){
-    (new \Tina4\GitDeploy())->log("Running a deployment");
-    (new \Tina4\GitDeploy())->doDeploy();
-});
